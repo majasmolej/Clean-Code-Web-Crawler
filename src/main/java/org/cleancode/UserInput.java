a0896cd5f53aa1a54e256758ad754986c45272a0
OlhaKupar <Olha.Kupar@infineon.com>
2024-04-03 12:48:59 +0200

    added methods to set and validate input from user + getter methods

--- src/main/java/org/cleancode/UserInput.java
+++ src/main/java/org/cleancode/UserInput.java
@@ -7,12 +7,48 @@ public class UserInput {
     private byte depth;
     private String targetLanguage;
 
+    private UserInputValidation userInputValidation = new UserInputValidation();
+    private Prompt prompt = new Prompt();
+    private Scanner scanner = new Scanner(System.in);
 
+    private void setURL (){
+        String userInputURL;
+        boolean isValidURL;
+        do{
+            userInputURL = scanner.next();
+            isValidURL= userInputValidation.validateURL(userInputURL);
+            if(!isValidURL){
+                System.out.println(prompt.getPromptReenterURL());
+            }
+        }while(!isValidURL);
+        this.URL=userInputURL;
+    }
 
+    private void setDepth(){
+        byte userInputDepth;
+        boolean isValidDepth;
+        do{
+            userInputDepth = scanner.nextByte();
+            isValidDepth = userInputValidation.validateDepth(userInputDepth);
+            if(!isValidDepth){
+                System.out.println(prompt.getPromptReenterDepth());
+            }
+        }while(!isValidDepth);
+        this.depth=userInputDepth;
+    }
 
-
-
-
+    private void setTargetLanguage(){
+        String userInputTargetLanguage;
+        boolean isValidTargetLanguage;
+        do{
+            userInputTargetLanguage = scanner.next();
+            isValidTargetLanguage = userInputValidation.validateTargetLanguage(userInputTargetLanguage);
+            if(!isValidTargetLanguage){
+                System.out.println(prompt.getPromptReenterTargetLanguage());
+            }
+        }while(!isValidTargetLanguage);
+        this.targetLanguage=userInputTargetLanguage;
+    }
 
     public String getURL (){
         return this.URL;
@@ -23,6 +59,7 @@ public class UserInput {
     public String getTargetLanguage(){
         return this.targetLanguage;
     }
+
 
 
 }
