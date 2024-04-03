0f419d53db9e515f1840944ce871a6e385a7c87b
OlhaKupar <Olha.Kupar@infineon.com>
2024-04-03 15:09:39 +0200

    changed acceptable depth range [0...2]

--- src/main/java/org/cleancode/Prompt.java
+++ src/main/java/org/cleancode/Prompt.java
@@ -5,7 +5,7 @@ public class Prompt {
     private final String promptURL = "Please enter a URL of a website, you wish to be crawled: ";
     private final String promptReenterURL = "Wrong URL format. Try again.";
     private final String promptDepth = "Please enter the crawl depth: ";
-    private final String promptReenterDepth = "The depth should be in the range from 1 to 3. Try again.";
+    private final String promptReenterDepth = "The depth should be in the range from 0 to 2. Try again.";
     private final String promptTargetLanguage = "Please enter the target language: ";
     private final String promptReenterTargetLanguage = "Unknown language. Try again.";
 
