7523530183caecefa15c0ae3cf197e6013dbada8
OlhaKupar <Olha.Kupar@infineon.com>
2024-04-03 12:14:47 +0200

    relocated method to validate user input into a separate class

--- src/main/java/org/cleancode/UserInput.java
+++ src/main/java/org/cleancode/UserInput.java
@@ -12,17 +12,7 @@ public class UserInput {
 
 
 
-    private boolean validateURL(String userInputURL){
-        return true;
-    }
-    private boolean validateDepth (byte userInputDepth){
-        return true;
-    }
 
-    //TODO: depending on the chosen API for translation, implement this method to accept only available target languages (and possibly add an extra method to convert them to a language code, f.e. "English" to "EN")
-    private boolean validateTargetLanguage (String userInputTargetLanguage){
-        return true;
-    }
 
     public String getURL (){
         return this.URL;
--- src/main/java/org/cleancode/UserInputValidation.java
+++ src/main/java/org/cleancode/UserInputValidation.java
@@ -1,4 +1,16 @@
 package org.cleancode;
 
 public class UserInputValidation {
+
+    private boolean validateURL(String userInputURL){
+        return true;
+    }
+    private boolean validateDepth (byte userInputDepth){
+        return true;
+    }
+
+    //TODO: depending on the chosen API for translation, implement this method to accept only available target languages (and possibly add an extra method to convert them to a language code, f.e. "English" to "EN")
+    private boolean validateTargetLanguage (String userInputTargetLanguage){
+        return true;
+    }
 }
