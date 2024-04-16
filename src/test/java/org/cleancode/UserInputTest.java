86b87076f7e3b2b18d59706e25a51b2e03425fde
majasmolej <smolej.9@gmail.com>
2024-04-16 23:20:07 +0200

    project testing set up

---
+++ src/test/java/org/cleancode/UserInputTest.java
@@ -0,0 +1,15 @@
+package org.cleancode;
+
+import org.junit.jupiter.api.BeforeEach;
+
+public class UserInputTest {
+
+    private UserInput userInput;
+
+    @BeforeEach
+    public void setUp() {
+        userInput = new UserInput();
+    }
+
+
+}
