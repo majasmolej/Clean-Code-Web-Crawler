4d482418819d53f311df032ca98268fd0a28bafb
OlgaKupar <Oljalya.topolja@gmail.com>
2024-04-28 15:40:39 +0200

    feat: establish connection with translation API

---
+++ src/main/java/org/cleancode/DeeplAPIWrapper.java
@@ -0,0 +1,68 @@
+package org.cleancode;
+
+import com.deepl.api.DeepLException;
+import com.deepl.api.Language;
+import com.deepl.api.TextResult;
+import com.deepl.api.Translator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DeeplAPIWrapper {
+
+    private final String API_AUTH_KEY = "ecac6743-0147-be5d-5240-0fe60c397b08:fx";
+
+    private Translator translator;
+
+    public DeeplAPIWrapper (){
+        this.translator=new Translator(API_AUTH_KEY);
+    }
+
+
+
+
+    public List<Language> getSupportedLanguages(){
+        List <Language> languageList = new ArrayList<>();
+        try {
+            languageList= translator.getTargetLanguages();
+
+        }catch (DeepLException | InterruptedException e){
+            ExceptionLogger.log(e);
+        }
+        return languageList;
+    }
+
+    //to check if the user´s input language is supported
+    public ArrayList<String> getSupportedLanguageNamesList(List <Language> languageList){
+        ArrayList<String> languageNamesList = new ArrayList<>();
+        for(Language language:languageList){
+            languageNamesList.add(language.getName());
+        }
+        return languageNamesList;
+    }
+
+    //return the code of the user´s input language
+    public String getLanguageCode (String inputLanguage) {
+        String languageCode=null;
+        List <Language> languages = getSupportedLanguages();
+        for(Language language: languages){
+            if(language.getName().equals(inputLanguage)) {
+                languageCode= language.getCode();
+            }
+        }
+        return languageCode;
+    }
+
+    public String getTranslatedHeading (String heading, String targetLanguageCode) throws Exception {
+        String translatedHeading = null;
+        try {
+            TextResult textResult = translator.translateText(heading, null, targetLanguageCode); //the source language is auto-detected
+            translatedHeading = textResult.getText();
+
+        } catch (DeepLException e) {
+            ExceptionLogger.log(e);
+        }
+        return translatedHeading;
+    }
+
+}
