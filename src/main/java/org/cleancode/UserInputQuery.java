aa35c7619d3b2eae5fffafbe17578ba311181ec6
OlgaKupar <Oljalya.topolja@gmail.com>
2024-04-28 16:21:30 +0200

    refactor: added getUserInputData and getUserInputDataSummary methods

--- src/main/java/org/cleancode/UserInputQuery.java
+++ src/main/java/org/cleancode/UserInputQuery.java
@@ -2,6 +2,38 @@ package org.cleancode;
 
 public class UserInputQuery {
 
+    private final StartingWebsite startingWebsite = new StartingWebsite();
+    private final Depth depth = new Depth();
+    private final TargetLanguage targetLanguage = new TargetLanguage();
+    private final Prompt prompt = new Prompt();
+    private final UserInputData userInputData = new UserInputData();
+
+    public UserInputData getUserInputData(){
+
+        prompt.getPromptStartingWebsite();
+        userInputData.startingWebsite = startingWebsite.getStartingWebsiteFromUser();
+
+        prompt.getPromptDepth();
+        userInputData.maxDepth = depth.getDepthFromUser();
+
+        prompt.getPromptTargetLanguage();
+        userInputData.targetLanguage = targetLanguage.getTargetLanguageFromUser();
+
+        return userInputData;
+    }
+
+    public String getUserInputDataSummary(){
+        return "_________________________________________________________________" +
+                "The user has selected the following options for the crawler: \n" +
+                "Starting website: "+userInputData.startingWebsite+"\n" +
+                "Crawling depth: "+userInputData.maxDepth+"\n" +
+                "Target language for translation: "+userInputData.targetLanguage+"."+
+                "________________________________________________________________";
+    }
+
+
+
+
 
 
 }
