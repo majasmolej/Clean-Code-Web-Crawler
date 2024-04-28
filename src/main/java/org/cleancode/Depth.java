c4abf8cb367c24ff1b72783e014a4c90a8560cd1
OlgaKupar <Oljalya.topolja@gmail.com>
2024-04-28 14:42:19 +0200

    refactor: depth handlich in a separate class

--- src/main/java/org/cleancode/Depth.java
+++ src/main/java/org/cleancode/Depth.java
@@ -1,4 +1,38 @@
 package org.cleancode;
 
+import java.util.Scanner;
 public class Depth {
+    Scanner scanner = new Scanner(System.in);
+    private Prompt prompt = new Prompt();
+
+    public byte getValidDepth(){
+        byte userInputDepth;
+        boolean isValidDepth;
+        do{
+            userInputDepth = scanner.nextByte();
+            String userInputDepthStr = Byte.toString(userInputDepth);
+            isValidDepth = isValidDepth(userInputDepthStr);
+            if(!isValidDepth){
+                System.out.println(prompt.getPromptReenterDepth());
+            }
+        }while(!isValidDepth);
+        return userInputDepth;
+    }
+
+    private boolean isValidDepth(String userInputDepth){
+        String regex = "[012]";
+        boolean result;
+        if(isNumeric(userInputDepth)){
+            result = userInputDepth.matches(regex);
+        }else{
+            result = false;
+        }
+        return result;
+    }
+
+    private boolean isNumeric(String userInputDepth){
+        String regex = "\\d";
+        return userInputDepth.matches(regex);
+    }
+
 }
