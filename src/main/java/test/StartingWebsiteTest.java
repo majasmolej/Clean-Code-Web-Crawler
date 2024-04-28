c3708fd9c1bff577d7415b84b199fa836fc27127
OlgaKupar <Oljalya.topolja@gmail.com>
2024-04-28 20:09:13 +0200

    refactor: added getUserInputWebSite, @Before and @After methods to eliminate duplicate code

--- src/main/java/test/StartingWebsiteTest.java
+++ src/main/java/test/StartingWebsiteTest.java
@@ -1,6 +1,8 @@
 package test;
 
 import org.cleancode.StartingWebsite;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.ByteArrayInputStream;
@@ -10,37 +12,32 @@ import static org.junit.Assert.*;
 
 public class StartingWebsiteTest {
 
+    private static StartingWebsite startingWebsite;
+
+    @Before
+    public void setUp(){
+        startingWebsite = new StartingWebsite();
+    }
+
+    @After
+    public void tearDown(){
+        startingWebsite=null;
+    }
+
     @Test
     public void getValidStartingWebsiteFromUser() {
-        InputStream originalSystemInput = System.in;
-        String testWebsite = "https://javatpoint.com\n";
-        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(testWebsite.getBytes());
-        System.setIn(byteArrayInputStream);
-
-        StartingWebsite startingWebsite = new StartingWebsite();
-        String url = startingWebsite.getStartingWebsiteFromUser();
+        String url = getUserInputWebSite("https://javatpoint.com");
         assertEquals("https://javatpoint.com", url);
-
-        System.setIn(originalSystemInput);
     }
 
     @Test
     public void getInvalidStartingWebsiteFromUser() {
-        InputStream originalSystemInput = System.in;
-        String testWebsite = "https://www.google.com\n";
-        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(testWebsite.getBytes());
-        System.setIn(byteArrayInputStream);
-
-        StartingWebsite startingWebsite = new StartingWebsite();
-        String url = startingWebsite.getStartingWebsiteFromUser();
+        String url = getUserInputWebSite("https://www.google.com");
         assertNotEquals("https://javatpoint.com", url);
-
-        System.setIn(originalSystemInput);
     }
 
     @Test
     public void prependHttpsNecessaryTest() {
-        StartingWebsite startingWebsite = new StartingWebsite();
         startingWebsite.setStartingUrl("www.google.com");
         startingWebsite.prependHttpsIfNecessary();
         assertEquals("https://www.google.com", startingWebsite.getStartingUrl());
@@ -48,14 +45,12 @@ public class StartingWebsiteTest {
 
     @Test
     public void prependHttpsNotNecessaryTest() {
-        StartingWebsite startingWebsite = new StartingWebsite();
         startingWebsite.setStartingUrl("https://www.google.com");
         startingWebsite.prependHttpsIfNecessary();
         assertEquals("https://www.google.com", startingWebsite.getStartingUrl());
     }
     @Test
     public void isValidWebsiteTest() {
-        StartingWebsite startingWebsite = new StartingWebsite();
         startingWebsite.setStartingUrl("https://www.google.com");
         boolean shouldBeValidWebsite = startingWebsite.isValidWebsite();
         assertTrue(shouldBeValidWebsite);
@@ -63,7 +58,6 @@ public class StartingWebsiteTest {
 
     @Test
     public void isNotValidWebsiteTest1() {
-        StartingWebsite startingWebsite = new StartingWebsite();
         startingWebsite.setStartingUrl("");
         boolean shouldNotBeValidWebsite = startingWebsite.isValidWebsite();
         assertFalse(shouldNotBeValidWebsite);
@@ -71,7 +65,6 @@ public class StartingWebsiteTest {
 
     @Test
     public void isNotValidWebsiteTest2() {
-        StartingWebsite startingWebsite = new StartingWebsite();
         startingWebsite.setStartingUrl("https://");
         boolean shouldNotBeValidWebsite = startingWebsite.isValidWebsite();
         assertFalse(shouldNotBeValidWebsite);
@@ -79,7 +72,6 @@ public class StartingWebsiteTest {
 
     @Test
     public void isNotValidWebsiteTest3() {
-        StartingWebsite startingWebsite = new StartingWebsite();
         startingWebsite.setStartingUrl("https://hello");
         boolean shouldNotBeValidWebsite = startingWebsite.isValidWebsite();
         assertFalse(shouldNotBeValidWebsite);
@@ -87,7 +79,6 @@ public class StartingWebsiteTest {
 
     @Test
     public void isNotValidWebsiteTest4() {
-        StartingWebsite startingWebsite = new StartingWebsite();
         startingWebsite.setStartingUrl("https://at");
         boolean shouldNotBeValidWebsite = startingWebsite.isValidWebsite();
         assertFalse(shouldNotBeValidWebsite);
@@ -95,9 +86,22 @@ public class StartingWebsiteTest {
 
     @Test
     public void isNotValidWebsiteTest5() {
-        StartingWebsite startingWebsite = new StartingWebsite();
         startingWebsite.setStartingUrl("https://||9");
         boolean shouldNotBeValidWebsite = startingWebsite.isValidWebsite();
         assertFalse(shouldNotBeValidWebsite);
+    }
+
+    private String getUserInputWebSite(String input){
+        InputStream originalSystemInput = System.in;
+        String testWebsite = input+"\n";
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(testWebsite.getBytes());
+        System.setIn(byteArrayInputStream);
+
+        StartingWebsite startingWebsite = new StartingWebsite();
+        String url = startingWebsite.getStartingWebsiteFromUser();
+
+        System.setIn(originalSystemInput);
+
+        return url;
     }
 }
