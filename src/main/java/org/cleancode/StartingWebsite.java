56b3b6a74787a75266dfc1dd48f448c2853dad08
OlgaKupar <Oljalya.topolja@gmail.com>
2024-04-28 15:10:38 +0200

    feat: added class to for startingWebsite

--- src/main/java/org/cleancode/StartingWebsite.java
+++ src/main/java/org/cleancode/StartingWebsite.java
@@ -1,4 +1,54 @@
 package org.cleancode;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
 public class StartingWebsite {
+
+    private String startingUrl;
+    private final Prompt prompt = new Prompt();
+
+    public String getStartingWebsiteFromUser() {
+        Scanner scanner = new Scanner(System.in);
+        do {
+            startingUrl = scanner.nextLine();
+            prependHttpsIfNecessary();
+            if(!isValidWebsite()){
+                System.out.println(prompt.getPromptReenterStartingWebsite());
+            }
+        } while (!isValidWebsite());
+        return startingUrl;
+    }
+
+    protected void prependHttpsIfNecessary() {
+        if (!startingUrl.startsWith("https://")) {
+            startingUrl = "https://" + startingUrl;
+        }
+    }
+
+    public boolean isValidWebsite() {
+        try {
+            URL url = new URL(startingUrl);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("HEAD"); // method GET would take longer
+            connection.setConnectTimeout(5000);
+            connection.setReadTimeout(5000);
+            int responseCode = connection.getResponseCode();
+            connection.disconnect();
+
+            // 200 <= responseCode < 400 indicates a successful connection
+            return (responseCode >= 200 && responseCode < 400);
+        } catch (MalformedURLException e) {
+            // Invalid URL format
+            System.out.println("This URL is malformed: " + e.getMessage());
+            return false;
+        } catch (IOException e) {
+            // Error connecting to the URL
+            System.out.println("There was an error connecting to the URL: " + e.getMessage());
+            return false;
+        }
+    }
 }
