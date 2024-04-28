d79cdbb197c53123ed69ec193e43e763b564bc35
OlgaKupar <Oljalya.topolja@gmail.com>
2024-04-28 23:12:01 +0200

    feat: added class to store data of a single website

---
+++ src/main/java/org/cleancode/Website.java
@@ -0,0 +1,13 @@
+package org.cleancode;
+
+import java.util.ArrayList;
+
+public class Website {
+
+    public String urlString;
+    public ArrayList<String> headings = new ArrayList<>();
+    public ArrayList<String> translatedHeadings = new ArrayList<>();
+    public ArrayList<String> functionalLinks = new ArrayList<>();
+    public ArrayList<String> brokenLinks = new ArrayList<>();
+
+}
